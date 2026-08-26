0e421793d5030ff53ea685a1febad9fcffb473f0
selli73 <aabulath@yandex.ru>
2026-08-26 23:02:30 +0300

    deleted swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  
-  const config = new DocumentBuilder()
-    .setTitle('Stream-overlay')
-    .setDescription('The stream-overlay API description')
-    .setVersion('1.0')
-    .addTag('overlay')
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
